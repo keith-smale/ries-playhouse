681303cafcbde1634f2b8f4590dffda0da3a9406
Keith Smale <keith.smale@codurance.com>
2018-05-07 21:13:48 +0100

    removed scrollspy from script

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -20,10 +20,4 @@
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
 })(jQuery); // End of use strict
